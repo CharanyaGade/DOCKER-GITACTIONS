38baff21de14957891236f82182e494dc408276c
Charanya Gade <charanyagade@gmail.com>
2025-10-14 20:00:45 +0530

    Updated ItemSpringBoot

--- ITEM-BACKEND/ItemSpringBootApp/src/main/java/com/klef/dev/ItemSpringBootAppApplication.java
+++ ITEM-BACKEND/ItemSpringBootApp/src/main/java/com/klef/dev/ItemSpringBootAppApplication.java
@@ -2,13 +2,19 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ItemSpringBootAppApplication {
+public class ItemSpringBootAppApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItemSpringBootAppApplication.class, args);
-		System.out.println("Running.........!");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ItemSpringBootAppApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ItemSpringBootAppApplication.class, args);
+        System.out.println("Running.........!");
+    }
 }
